db352f9fc83ab9309069e6e26ef4dde5f962d3f6
wenderson-vostro <wendersonj@hotmail.com>
2020-07-01 14:57:03 -0300

    atualizacao do codigo
    -convertido para a ultima versao funcional (carla_sync_dqn)
    -editado para funcionar usando apenas tick
    -demais prints corrigidos

--- dqn_sync.py
+++ dqn_sync.py
@@ -1,9 +1,8 @@
-'''
+"""
 Créditos do código base da DQN ao autor: ??, 2018
 DQN modificada por Wenderson Souza
-Ambiente preparado por Wenderson Souza
-'''
-
+Ambiente preparado por Wenderson Souza, baseado no código base da OpenGymIA
+"""
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 
@@ -13,10 +12,10 @@ import sys
 from time import sleep
 import math
 from random import randrange
+
 try:
-    sys.path.append(
-        glob.glob('../../carla/dist/carla-0.9.7-py3.5-linux-x86_64.egg')[0])
-    sys.path.append(glob.glob('../../PythonAPI/carla/')[0])
+    sys.path.append(glob.glob("../../carla/dist/carla-0.9.7-py3.5-linux-x86_64.egg")[0])
+    sys.path.append(glob.glob("../../PythonAPI/carla/")[0])
 except IndexError:
     pass
 
@@ -30,11 +29,24 @@ from carla import ColorConverter as cc
 from collections import deque, Counter
 from datetime import datetime
 
-
+# Variáveis Simulacao
 camera_size_x = 800
 camera_size_y = 600
 qtd_acoes = 7
-####
+# Variáveis DQN
+epsilon = 0.5
+eps_min = 0.05
+eps_max = 1.0
+eps_decay_steps = 500000
+buffer_len = 20000  # limita a quantidade de experiencias. quando encher, retira as ultimas experiencias
+exp_buffer = deque(maxlen=buffer_len)
+num_episodes = 100
+batch_size = 32
+learning_rate = 0.001
+discount_factor = 0.97
+# Variáveis de mundo e cliente
+world = None
+client = None
 
 
 class Env(object):
@@ -43,74 +55,84 @@ class Env(object):
         self.reward = 0
         self.done = 0
         self.info = None
-        self.world = world
         self.player = None
+        self.dict_act = [
+            "sem acao",
+            "frente",
+            "frente-direita",
+            "frente-esquerda",
+            "ré/freio",
+            "ré-direita",
+            "ré-esquerda",
+        ]
 
     def reset(self):
         self.reward = 0
         self.done = 0
-        self.world.restart()
-        self.player = self.world.player
-        self.world.camera_sensor.listen(
-            lambda img: self.world.getCameraImage(img, self))
+        world.restart()
+        self.player = world.player
+        world.camera_sensor.listen(lambda img: world.convertImage(img, env=self))
         print("Ator resetado...")
 
     def applyReward(self):
         # se houve colisão, negativa em X pontos e termina
-        if self.world.colission_history > 0:
+        if world.colission_history > 0:
             self.reward = self.reward - 10
             self.done = 1
             return
 
         # se esta perto do objetivo, perde menos pontos
-        dist_atual = self.world.destiny_dist()
-        self.reward = self.reward + \
-            (dist_atual - self.world.last_distance)/dist_atual
-        self.world.last_distance = dist_atual
+        dist_atual = world.destiny_dist()
+        self.reward = self.reward + (dist_atual - world.last_distance) / dist_atual
+        world.last_distance = dist_atual
 
-        vel = self.world.velocAtual()
-        #if(vel >= 0):
-        self.reward = self.reward + (vel/(vel+1))
+        if dist_atual <= 10:  # se esta a menos de 10unidades do destino, fim.
+            self.reward = self.reward + 100
+            self.done = 1
+            return
+        vel = world.velocAtual()
 
     def step(self, action):
-
         self.info = self.applyAction(action)
-
-        #self.observation = getObservation(self.world.getObservation())
+        world.carla_world.tick()  # atualiza o mundo
         self.applyReward()
-
         return self.observation, self.reward, self.done, self.info
 
     def applyAction(self, action):
         speed_limit = 10
-        if self.world.velocAtual() >= speed_limit:
+        if world.velocAtual() >= speed_limit:
             throttle = 0.0
-            print("\nAcima do limite de velocidade de", speed_limit, "km/h...")
+            print("\nAcima do limite de velocidade de ", speed_limit, "km/h...")
         else:
             throttle = 0.5
         actions = (
             (0.0, 0.0, False),  # sem acao
-
             (throttle, 0.0, False),  # frente
             (throttle, -0.5, False),  # frente-direita
             (throttle, 0.5, False),  # frente-esquerda
-
             (throttle, 0.0, True),  # ré/freio
             (throttle, -0.5, True),  # ré-direita
-            (throttle, 0.5, True)  # ré-esquerda
+            (throttle, 0.5, True),  # ré-esquerda
         )  # 7 acoes
 
         print("\tAção: ", actions[action])
-
-        self.player.apply_control(carla.VehicleControl(
-            actions[action][0], actions[action][1], reverse=actions[action][2]))
+        self.player.apply_control(
+            carla.VehicleControl(
+                actions[action][0], actions[action][1], reverse=actions[action][2]
+            )
+        )
         return None
+
+    def getObservation(self):
+        return self.observation
 
 
 class World(object):
     def __init__(self, carla_world):
-        self.world = carla_world
-        self.map = self.world.get_map()
+        """
+        Incialização das variáveis
+        """
+        self.carla_world = carla_world
         self.player = None
         self.collision_sensor = None
         self.camera_sensor = None
@@ -121,72 +143,85 @@ class World(object):
 
     def spawnPlayer(self):
         # Cria um audi tt no ponto primeiro waypoint dos spawns
-        blueprint = (
-            self.world.get_blueprint_library().find('vehicle.audi.tt'))
-        if blueprint.has_attribute('color'):
-            color = random.choice(
-                blueprint.get_attribute('color').recommended_values)
-            blueprint.set_attribute('color', color)
+        blueprint = self.carla_world.get_blueprint_library().find("vehicle.audi.tt")
+        if blueprint.has_attribute("color"):
+            color = random.choice(blueprint.get_attribute("color").recommended_values)
+            blueprint.set_attribute("color", color)
         # Spawn the player.
-        spawn_point = (self.world.get_map().get_spawn_points())[0]
-        self.player = self.world.try_spawn_actor(blueprint, spawn_point)
+        spawn_point = (self.carla_world.get_map().get_spawn_points())[0]
+        self.player = self.carla_world.try_spawn_actor(blueprint, spawn_point)
         # para nao comecar as ações sem ter iniciado adequadamente
-        time.sleep(2)
+
+        """
+        prepara o carro
+        """
+        for i in range(100):
+            self.carla_world.tick()
 
     def restart(self):
         # Set up the sensors.
+        print("Reiniciando ...")
         self.destroy()
+        self.config_carla_world()
         self.spawnPlayer()
         self.config_camera()
         self.config_collision_sensor()
+        print("Fim do Reinício.")
         print("Iniciando componentes do ator...")
 
+    def config_carla_world(self):
+        """
+        Configuração do World
+        -Será síncrono e quando atualizar, atualizará 20 frames.
+        """
+        settings = self.carla_world.get_settings()
+        settings.synchronous_mode = True
+        settings.fixed_delta_seconds = 0.05  # quando tick for usado, a simulação irá simular 1/fixed_delta_seconds frames. Em 0.05 serão 20 frames por tick; 0.5 serão 2 frames por tick. Usar sempre tempo < 0.1
+        self.carla_world.apply_settings(settings)
+
     def config_collision_sensor(self):
-        bp = self.world.get_blueprint_library().find('sensor.other.collision')
-        self.collision_sensor = self.world.spawn_actor(
-            bp, carla.Transform(carla.Location(x=2.0, z=1.0)), attach_to=self.player)
+        bp = self.carla_world.get_blueprint_library().find("sensor.other.collision")
+        self.collision_sensor = self.carla_world.spawn_actor(
+            bp, carla.Transform(carla.Location(x=2.0, z=1.0)), attach_to=self.player
+        )
         self.collision_sensor.listen(lambda event: self.on_collision(event))
 
     def on_collision(self, event):
         self.colission_history += 1
-        print("\tMais uma colisão...")
+        print("\n|| Mais uma colisão... || ")
 
     def config_camera(self):
-        camera_bp = self.world.get_blueprint_library().find('sensor.camera.rgb')
+        camera_bp = self.carla_world.get_blueprint_library().find("sensor.camera.rgb")
         camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
-        camera_bp.set_attribute('image_size_x', str(camera_size_x))
-        camera_bp.set_attribute('image_size_y', str(camera_size_y))
-        # Captura uma imagem a cada 50hz = 0.02
-        #camera_bp.set_attribute('sensor_tick', '1')
-        camera_bp.set_attribute('fov', '110')  # angulo horizontal de 110graus
-        self.camera_sensor = self.world.spawn_actor(
-            camera_bp, camera_transform, attach_to=self.player)
-
-    def tick(self):
-        time.sleep(0.5)
+        camera_bp.set_attribute("image_size_x", str(camera_size_x))
+        camera_bp.set_attribute("image_size_y", str(camera_size_y))
+        self.camera_sensor = self.carla_world.spawn_actor(
+            camera_bp, camera_transform, attach_to=self.player
+        )
 
     def destroy(self):
-        print("Destruindo ator e sensores...")
-        actors = [
-            self.camera_sensor,
-            self.collision_sensor,
-            self.player]
+        settings = self.carla_world.get_settings()
+        settings.synchronous_mode = False
+        self.carla_world.apply_settings(
+            settings
+        )  # Perde o controle manual da simulação
+        print("Destruindo atores e sensores...")
+        actors = [self.camera_sensor, self.collision_sensor, self.player]
         for actor in actors:
             if actor is not None:
                 actor.destroy()
 
-    def getCameraImage(self, image, env):
-        '''
-        é realizado automaticamento 
+    def convertImage(self, image, env):
+        """
+        é realizado automaticamente 
         quando há uma nova imagem disponível pelo sensor camera_sensor.listen
-        '''
-        image.convert(cc.Raw)
+        """
+        # image.convert(cc.Raw)
         array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
         array = np.reshape(array, (image.height, image.width, 4))
         array = array[:, :, :3]
         array = array[:, :, ::-1]
         array = np.expand_dims(array, axis=0)
-        
         env.observation = array  # repassa para o ambinte uma nova imagem da camera
 
     def defineDestiny(self, d):
@@ -199,26 +234,13 @@ class World(object):
 
     def velocAtual(self):
         v = self.player.get_velocity()
-        vel = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
+        vel = int(3.6 * math.sqrt(v.x ** 2 + v.y ** 2 + v.z ** 2))
         return vel
 
-epsilon = 0.5
-eps_min = 0.05
-eps_max = 1.0
-eps_decay_steps = 500000
-
-buffer_len = 20000
-# limita a quantidade de experiencias. quando encher, retira as ultimas experiencias
-exp_buffer = deque(maxlen=buffer_len)
-
-num_episodes = 800
-batch_size = 48
-learning_rate = 0.001
-discount_factor = 0.97
 
 def epsilon_greedy(action, step):
     p = np.random.random(1).squeeze()
-    epsilon = max(eps_min, eps_max - (eps_max-eps_min) * step/eps_decay_steps)
+    epsilon = max(eps_min, eps_max - (eps_max - eps_min) * step / eps_decay_steps)
     if np.random.rand() < epsilon:
         return np.random.randint(qtd_acoes)
     else:
@@ -228,74 +250,79 @@ def epsilon_greedy(action, step):
 def sample_memories(batch_size):
     perm_batch = np.random.permutation(len(exp_buffer))[:batch_size]
     mem = np.array(exp_buffer)[perm_batch]
-    return mem[:, 0], mem[:, 1], mem[:, 2], mem[:, 3], mem[:, 4]
+    return mem[:, 0][0], mem[:, 1][0], mem[:, 2][0], mem[:, 3][0], mem[:, 4][0]
 
 
 def generateNetwork(scope):
-    #with tf.variable_scope(name_scope) as scope:
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(
-        camera_size_y, camera_size_x, 3)))  # para usar imagem gray, tem que trocar o shape da imagem na layer. X e Y trocados
+    model.add(
+        layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(camera_size_y, camera_size_x, 3)
+        )
+    )  # para usar imagem gray, tem que trocar o shape da imagem na layer. X e Y trocados
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(64, (3, 3), activation="relu"))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(64, (3, 3), activation="relu"))
     model.add(layers.Flatten())
-    model.add(layers.Dense(128, activation='softmax'))
+    model.add(layers.Dense(128, activation="softmax"))
     model.add(layers.Dense(qtd_acoes))
-    # model.summary() #display architecture
-    model.compile(optimizer='sgd',
-                  loss='mean_squared_error',
-                  metrics=['accuracy'])
+    model.compile(optimizer="sgd", loss="mean_squared_error", metrics=["accuracy"])
     return model
 
-mainQ = generateNetwork('mainQ')
-targetQ = generateNetwork('targetQ')
 
-action = 0
-y=0
-
-def main():
-    world = None
+def connect(world, client, ip="localhost"):
     try:
         print("Tentando conectar ao servidor Carla...")
-        client = carla.Client('127.0.0.1', 2000)
-
+        client = carla.Client(ip, 2000)
         client.set_timeout(1.0)
-        print("Conectado com sucesso.")
+        carla_world = World(client.get_world())
+    except RuntimeError:
+        print("Falha na conexão.")
+        return None, None
+    print("Conectado com sucesso.")
+    return carla_world, client
 
-        world = World(client.get_world())
+
+def main():
+    try:
         env = Env(world)
         print("Iniciando episodios...")
-
         env.reset()
+        print("Inicializando DQN...")
 
         global_step = 0
-        copy_steps = 100
-        steps_train = 4
-        start_steps = 2000
+        copy_steps = (
+            10  # a cada x passos irá copiar a main_network para a target_network
+        )
+        steps_train = 5  # a cada x passos irá treinar a main_network
+        start_steps = 20  # passos inicias . default=200
 
-        # for each episode
+        print("Gerando rede DQN principal...")
+        # mainQ = generateNetwork('mainQ')
+        print("Gerando rede DQN alvo...")
+        # targetQ = generateNetwork('targetQ')
+        print("Redes geradas.")
+
+        action = 0
+        y = 0
+
         for i in range(num_episodes):
-            env.world.tick()
+            print("\n%Episodio", num_episodes, "%")
             done = 0
             info = None
-            obs = env.observation  # env.reset()
-            epoch = 0
+            world.carla_world.tick()  # atualiza o mundo
+            obs = env.getObservation()
+            passos_ep = 0  # quantidade de passos realizados em um episodio
             episodic_reward = 0
             actions_counter = Counter()
             episodic_loss = []
 
             while not done:
-
-                # get the preprocessed game screen
-                obs = env.observation
-                # feed the game screen and get the Q values for each action
+                print("| Passo(total)", global_step, " - Início. ")
                 actions = mainQ.predict(obs)
-
                 # get the action
                 # escolhe a posicao com maior probabilidade
-                #action.assign(np.argmax(actions))
                 action = np.argmax(actions)
                 actions_counter[str(action)] += 1
 
@@ -307,56 +334,74 @@ def main():
 
                 # Store this transistion as an experience in the replay buffer
                 exp_buffer.append([obs, action, next_obs, reward, done])
+                print("Terminou o passo com recompensa: ", reward, " .|\n")
 
                 # After certain steps, we train our Q network with samples from the experience replay buffer
                 if global_step % steps_train == 0 and global_step > start_steps:
-
+                    print("\n-- Atualização da Q-Network %100 passos --")
                     # sample experience
-                    obs, act, next_obs, reward, done = sample_memories(
-                        batch_size)
+                    obs, act, next_obs, reward, done = sample_memories(batch_size)
 
-                    # TALVEZ PRECISE USAR POR CONTA DO SHAPE DA OBS::
-                    # obs = np.expand_dims(obs, axis=0) #transforma as observacoes em um array
-                    # next_obs= np.expand_dims(obs, axis=0) #transforma as observacoes em um array
                     # valor de probabilidade da ação mais provável
                     targetValues = targetQ.predict(next_obs)
+                    print("\nValores alvo: ", targetValues)
+
                     bestAction = np.argmax(targetValues)
+                    print("\nMelhor Ação: ", bestAction)
 
-                    y = reward + discount_factor * \
-                        np.max(targetValues) * (1 - done)
-                    targetValues[bestAction] = y
+                    y = reward + discount_factor * np.max(targetValues) * (1 - done)
+                    print("\nY atualizado: ", y)
+                    print(
+                        "\nAntes (melhor ação): ",
+                        targetValues[0][bestAction],
+                        "| Depois: ",
+                        y,
+                    )
+
+                    targetValues[0][bestAction] = y
                     # now we train the network and calculate loss
-                    # train mode
-
                     # gradient descent (x=obs e y=recompensa)
-
                     train_loss = mainQ.fit(obs, targetValues)
                     episodic_loss.append(train_loss)  # historico
+                    print("|| Loss Episódica: ", episodic_loss)
 
-                # after some interval we copy our main Q network weights to target Q network
-                if (global_step+1) % copy_steps == 0 and global_step > start_steps:
-                    # Copy networks weights
+                if (global_step + 1) % copy_steps == 0 and global_step > start_steps:
+                    # Cópia dos pesos da rede principal para a rede alvo.
                     targetQ.set_weights(mainQ.get_weights())
 
-                obs = next_obs
-                epoch += 1
+                obs = next_obs  # troca a u
+                passos_ep += 1
                 global_step += 1
                 episodic_reward += reward
-
-            print('Epoch', epoch, 'Reward', episodic_reward,)
+            print(
+                "\nEpisódio ",
+                num_episode,
+                "com ",
+                epoch,
+                "passos and recompensa total: ",
+                episodic_reward,
+                ".",
+            )
 
     except RuntimeError:
         print("\n\ntreta: RuntimeError")
         # traceback.print_exc()
         pass
-
+    except Exception:
+        traceback.print_exc()
+        pass
     finally:
-
         if world is not None:
             world.destroy()
 
+"""
+Conexão com o simulador
+"""
+world, client = connect(world, client)  # , ip='35.197.43.29')
 
-if __name__ == '__main__':
+print("Pronto para iniciar.")
+
+if __name__ == "__main__" and world != None and client != None:
     main()
-
-
+else:
+    print("Erro. Verifique a conexão.")
